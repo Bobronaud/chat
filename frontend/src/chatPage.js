21edda6b3c4e1295ee159bfc5e6b362ec37d96da
Alexander <bobronaud@gmail.com>
2023-10-17 14:20:38 +0300

    create chat side: mapping messages and message typing

--- frontend/src/chatPage.js
+++ frontend/src/chatPage.js
@@ -34,22 +34,63 @@ const ChatPage = () => {
         });
       });
   }, [dispatch]);
+  const handlerTypeText = () => {};
+  const handlerSendMessage = () => {};
   return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ width: 300 }}>
-      <p className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <span className="fs-4">Каналы</span>
-      </p>
-      <hr />
-      <ul className="nav nav-pills flex-column mb-auto">
-        {channels.map(({ name, id }) => {
-          const classes = active === id ? 'nav-link active' : 'nav-link link-dark';
-          return (
-            <li key={id} className="nav-item">
-              <p className={classes}>{name}</p>
+    <div className="row">
+      <div className="col-sm-3 mb-4 mb-md-0">
+        <p className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+          <span className="fs-4">Каналы</span>
+        </p>
+        <hr />
+        <ul className="nav nav-pills flex-column mb-auto">
+          {channels.map(({ name, id }) => {
+            const classes = active === id ? 'nav-link active' : 'nav-link link-dark';
+            return (
+              <li key={id} className="nav-item">
+                <p className={classes}>{name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="col-sm-6">
+        <ul className="list-unstyled">
+          {messages.map((m, i) => (
+            <li key={i} className="d-flex justify-content-between mb-4">
+              <div className="card">
+                <div className="card-header d-flex justify-content-between p-3">
+                  <p className="fw-bold mb-0">Name here</p>
+                  <p class="text-muted small mb-0">past time here</p>
+                </div>
+                <div class="card-body">
+                  <p class="mb-0">{m}</p>
+                </div>
+              </div>
             </li>
-          );
-        })}
-      </ul>
+          ))}
+          <li className="bg-white mb-3">
+            <div className="form-outline">
+              <textarea
+                placeholder="Type your message..."
+                className="form-control"
+                id="textArea"
+                rows="4"
+                style={{ resize: 'none' }}
+              ></textarea>
+              <label className="form-label" htmlFor="textArea"></label>
+            </div>
+          </li>
+          <button
+            onChange={handlerTypeText}
+            onClick={handlerSendMessage}
+            type="button"
+            class="btn btn-info btn-rounded float-end"
+          >
+            Send
+          </button>
+        </ul>
+      </div>
     </div>
   );
 };
