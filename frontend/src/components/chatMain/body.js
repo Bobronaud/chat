74c2ad7e17909f1b1671baf637f15f35302ea30a
Alexander <bobronaud@gmail.com>
2023-11-04 13:28:45 +0300

    add censore chat

--- frontend/src/components/chatMain/body.js
+++ frontend/src/components/chatMain/body.js
@@ -1,15 +1,23 @@
 import { useSelector } from 'react-redux';
+import filter from 'leo-profanity';
 
 const Body = () => {
   const { messages } = useSelector((state) => state.messages);
   const { active } = useSelector((state) => state.channels);
+  const censored = (text) => {
+    filter.loadDictionary('en');
+    const censoredByEn = filter.clean(text);
+    filter.loadDictionary('ru');
+    return filter.clean(censoredByEn);
+  };
+
   return (
     <div id="messages-box" className="chat-messages overflow-auto px-5 ">
       {messages
         .filter(({ channelId }) => channelId === active)
         .map(({ body, id, username }) => (
           <div key={id} className="text-break mb-2">
-            <b>{username}</b>: {body}
+            <b>{username}</b>: {censored(body)}
           </div>
         ))}
     </div>
