72a9623e5260a9d717497117ccb5a4919b73920e
root <root@DESKTOP-1K3Q23K>
2023-11-28 21:38:12 +0300

    add useMemo hook to autorizationApi

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/login.js';
 import Signup from './pages/signup.js';
@@ -15,31 +15,31 @@ const App = () => {
 
   const [authData, setAuthData] = useState(initialAuthData);
 
-  const autorizationApi = {
-    authData,
-    login(token, username) {
-      window.localStorage.setItem('token', token);
-      window.localStorage.setItem('username', username);
-      setAuthData({token, username, isAuthorization: true});
-    },
-    logout() {
-      window.localStorage.removeItem('token');
-      window.localStorage.removeItem('username');
-      setAuthData({token: null, username: null, isAuthorization: false});
-    },
-  };
+  const autorizationApi = useMemo(() => ({
+      authData,
+      login(token, username) {
+        window.localStorage.setItem('token', token);
+        window.localStorage.setItem('username', username);
+        setAuthData({ token, username, isAuthorization: true });
+      },
+      logout() {
+        window.localStorage.removeItem('token');
+        window.localStorage.removeItem('username');
+        setAuthData({ token: null, username: null, isAuthorization: false });
+      },
+    }), [authData]);
 
   return (
-  <AutorizationContext.Provider value={autorizationApi}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="/" element={<ChatMain />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </AutorizationContext.Provider>
+    <AutorizationContext.Provider value={autorizationApi}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="/" element={<ChatMain />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AutorizationContext.Provider>
   );
 };
 
